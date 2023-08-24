53732ed2aac8cbd639aecbc149528d6826748cc8
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-24 07:51:59 -0300

    React connect locally to spring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig'
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [petshops, setPetshops] = useState ();
+
+  const getPetshops = async () => {
+
+    try{
+      const response = await api.get("/petshop");
+
+      console.log(response.data);
+    
+      setPetshops(response.data);}
+    catch (err){
+      console.log(err);}
+  };
+
+  useEffect(()=> {
+    getPetshops();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
-
 export default App;
