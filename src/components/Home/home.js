ab52702c112df7b474a883c7e090c928310a4879
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-25 12:58:23 -0300

    Home v2, refactor to use bootstrap more efficiently

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import Logo from '../Logo/logo';
 import DTILogo from '../DTILogo/dtilogo';
 import './home.css';
@@ -7,31 +7,46 @@ import './home.css';
 function Home() {
   return (
     <Container className="home-content">
+      <br />
       <Row>
-        <Col md={4}>
+        <Col md={4} className="img-col">
           <Logo className="home-img" />
         </Col>
         <Col md={4}>
-          <h1>Petshop - DTI Digital</h1>
+          <Row>
+            <h1 className="text-center">Petshop</h1>
+          </Row>
+          <Row>
+            <h1 className="text-center">DTI Digital</h1>
+          </Row>
         </Col>
-        <Col md={4}>
+        <Col md={4} className="img-col">
           <DTILogo className="home-img" />
         </Col>
       </Row>
       <Row>
         <Col>
-          <h2>Descrição do Problema:</h2>
-          <p>
-            Você pode inserir a data do banho, a quantidade de cães pequenos e a quantidade de cães grandes e clicar em "Encontrar" para descobrir qual petshop oferece o melhor preço com base na proximidade e custo.
-          </p>
+          <Card>
+            <Card.Body>
+              <Card.Title>Descrição do Problema:</Card.Title>
+              <Card.Text>
+              O desafio consiste em encontrar, entre um conjunto de petshops, aquele que melhor atende aos requisitos específicos. O objetivo principal é minimizar os custos totais do banho, considerando que cada petshop tem preços diferentes para cães pequenos e grandes, que variam de acordo com os dias da semana. Além disso, em caso de empate nos preços, a proximidade do petshop em relação ao canil deve ser considerada para tomar a decisão final.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
+      <br></br>
       <Row>
         <Col>
-          <h2>Como Usar o Sistema:</h2>
-          <p>
-            Você pode inserir a data do banho, a quantidade de cães pequenos e a quantidade de cães grandes e clicar em "Encontrar" para descobrir qual petshop oferece o melhor preço com base na proximidade e custo.
-          </p>
+          <Card>
+            <Card.Body>
+              <Card.Title>Como Usar o Sistema:</Card.Title>
+              <Card.Text>
+                Você pode inserir a data do banho, a quantidade de cães pequenos e a quantidade de cães grandes e clicar em "Encontrar" para descobrir qual petshop oferece o melhor preço com base na proximidade e custo.
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
