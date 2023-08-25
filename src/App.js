3174ab080deabd91e10cba83e14d617bf6d91745
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-25 18:37:34 -0300

    App and index main css and layout changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import BestPetShopPage from './components/bestPetshopPage';
+import BestPetShopPage from './components/BestPetshopPage/bestPetshopPage';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Header from './components/Header/header';
 import Home from './components/Home/home';
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Router>
-      <div className="App">
+      <div className="App" >
         <Header/>
         <Routes>
           <Route path="/best-petshop" element={<BestPetShopPage />} />
