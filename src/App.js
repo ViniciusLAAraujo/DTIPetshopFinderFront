95a287b1b318942f2b1405962a62049ed073b272
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2023-08-25 12:54:47 -0300

    Removing tests from App.js, general cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import './App.css';
-import api from './api/axiosConfig'
-import {useState, useEffect} from 'react';
 import BestPetShopPage from './components/bestPetshopPage';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Header from './components/Header/header';
 import Home from './components/Home/home';
+import AllPetshopsPage from './components/allPetshopPage';
 
 function App() {
-
-  const [petshops, setPetshops] = useState ();
-
-  const getPetshops = async () => {
-
-    try{
-      const response = await api.get("/petshop");
-      setPetshops(response.data);}
-    catch (err){
-      console.log(err);}
-  };
-
-  useEffect(()=> {
-    getPetshops();
-  },[])
 
   return (
     <Router>
       <div className="App">
         <Header/>
         <Routes>
-          <Route path="/best-petshop" element={<BestPetShopPage petshops={petshops} />} />
+          <Route path="/best-petshop" element={<BestPetShopPage />} />
+          <Route path="/all-petshops" element={<AllPetshopsPage />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
